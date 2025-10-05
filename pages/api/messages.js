df4071528ec7a419aa8ccd35a61fa242ba148c67
oridekel1208-ops <oridekel1208@gmail.com>
2025-10-05 10:49:24 +0300

    Update messages.js

--- pages/api/messages.js
+++ pages/api/messages.js
@@ -1,12 +1,7 @@
-// pages/api/messages.js
 import { getRecentMessages } from '../../lib/db.js';
 
 export default async function handler(req, res) {
-  try {
-    const messages = await getRecentMessages(200);
-    return res.status(200).json(messages);
-  } catch (err) {
-    console.error('messages API error', err);
-    return res.status(500).json({ error: err.message || String(err) });
-  }
+  if (req.method !== 'GET') return res.status(405).end('Method Not Allowed');
+  const messages = await getRecentMessages();
+  res.status(200).json(messages);
 }
