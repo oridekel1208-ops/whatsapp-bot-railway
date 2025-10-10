91a42be67cf6ad0b44ba82afd9b9cf530f16924b
oridekel1208-ops <oridekel1208@gmail.com>
2025-10-10 09:34:00 +0300

    Create bots.js

---
+++ pages/api/debug/bots.js
@@ -0,0 +1,11 @@
+import { pool } from "../../../lib/db.js";
+
+export default async function handler(req, res) {
+  try {
+    const bots = await pool.query("SELECT * FROM bots");
+    const clients = await pool.query("SELECT * FROM clients");
+    res.status(200).json({ bots: bots.rows, clients: clients.rows });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
