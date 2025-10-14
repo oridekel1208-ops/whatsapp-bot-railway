2fd8b6b4bd353c86028934f0f116a691b342b3e0
oridekel1208-ops <oridekel1208@gmail.com>
2025-10-14 14:00:03 +0300

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,5 +1,5 @@
 // pages/api/webhook.js
-import { getClientByPhoneNumberId, getBotByClientId, insertMessage } from "../../../lib/db";
+import { getClientByPhoneNumberId, getBotByClientId, insertMessage } from "../../lib/db";
 
 export default async function handler(req, res) {
   try {
